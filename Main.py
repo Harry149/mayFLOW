007795c99e1e63d280e8101d7861748ae3812057
Harry149 <70582632+Harry149@users.noreply.github.com>
2023-02-04 11:28:44 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -39,7 +39,9 @@ webhook = "https://discord.com/api/webhooks/1070321788392849459/dHjJvJpxwSV1PtdZ
 async def on_ready():
     guild = client.get_guild(1068884996871430255)
     if guild:
-        await guild.me.edit(nick='JudiciaryFLOW', avatar=discord.File('pfp.png'))
+        await client.user.edit(username='JudiciaryFLOW')
+        with open('pfp.png', 'rb') as f:
+            await client.user.edit(avatar=f.read())
     print(f"Logged in as {client.user}")
     await client.change_presence(status=discord.Status.dnd, activity=discord.Game(name=" With Cleo"))
 
